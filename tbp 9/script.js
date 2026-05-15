8dd393e72f0dbdff9826bff84305834302666faf
osoagacblg <akbashuseyin3883@gmail.com>
2026-05-15 20:42:21 +0300

    Add files via upload

---
+++ tbp 9/script.js
@@ -0,0 +1,28 @@
+const agaclar = [
+    {
+        ad: "Ejder Ağacı (Dracaena Marginata)",
+        bilgi: "3-5 yaş aralığındadır.Kılıç şeklinde, ince, uzun ve genellikle kenarları kırmızı/mor şeritli yaprakları vardır.İnce ve esnek bir gövdeye sahiptir. Bitki büyüdükçe alt yapraklar dökülür ve gövde odunsu bir hal alır.NASA'nın temiz hava çalışmasına göre, formaldehit ve benzen gibi iç mekan hava kirleticilerini temizleme özelliğiyle bilinir.Doğrudan güneş ışığı yerine aydınlık ama dolaylı ışığı sever. Az suyla yetinebilen, dayanıklı bir bitkidir.",
+        resim: "images/agac9.jpg"
+    },
+];
+
+function getId() {
+    const params = new URLSearchParams(window.location.search);
+    return parseInt(params.get("id")) || 1;
+}
+
+function agaciGoster(id) {
+    const agac = agaclar[id - 1];
+    document.getElementById("agacAdi").innerText = agac.ad;
+    document.getElementById("agacBilgi").innerText = agac.bilgi;
+    document.getElementById("agacResim").src = agac.resim;
+}
+
+function sonrakiAgac() {
+    let id = getId();
+    id++;
+    if (id > agaclar.length) id = 1;
+    window.location.search = "?id=" + id;
+}
+
+agaciGoster(getId());
